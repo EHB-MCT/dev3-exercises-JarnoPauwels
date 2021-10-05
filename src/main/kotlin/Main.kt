d4c3947a55d22e636119cfab0d1aa3199acaa72d
JarnoPauwels <71709292+JarnoPauwels@users.noreply.github.com>
2021-10-05 16:47:40 +0200

    Hello World Dynamisch

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+    fun main() {
+        val names = arrayOf("Quinn", "Geert", "Kurt", "Bart", "Piet")
+
+        for (name in names) {
+            greet(name)
+        }
+    }
+
+    fun greet(name: String) {
+        println("Hello $name")
+    }
