a468653789237c0bcd3ed76ae95f6aa82627ae01
JarnoPauwels <71709292+JarnoPauwels@users.noreply.github.com>
2021-10-12 16:48:14 +0200

    WIP Challenge Two

--- session2/adventure-game/src/main/kotlin/Main.kt
+++ session2/adventure-game/src/main/kotlin/Main.kt
@@ -8,9 +8,9 @@ fun main(){
 fun challengeOne(){
     val correctAnswer = listOf("One", "Two")
 
-    println("Solve this riddle")
+    println("For you're first challenge you will have to solve this riddle!")
     Thread.sleep(1000)
-    println("What is ...")
+    println("What is ...? ")
     val userAnswer = readLine()!!
     determineWinner(userAnswer, correctAnswer )
 }
@@ -18,12 +18,40 @@ fun challengeOne(){
 fun determineWinner(userAnswer: String?, correctAnswer: List<String>){
     if (correctAnswer.contains(userAnswer)) {
         println("You won")
-        Thread.sleep(500)
+        Thread.sleep(1000)
         challengeTwo()
     } else {
         gameOver()
     }
+}
 
+// Solve a riddle
+fun challengeTwo(){
+    //val low = arrayOf(1, 2, 3)
+    //val high = arrayOf(4, 5, 6)
+
+
+    println("For you're second challenge you will have to predict a dice roll!")
+    Thread.sleep(1000)
+    println("Pick between 'low' and 'high'")
+    val userAnswer = readLine()!!
+    rollDice()
+    //if (readLine() == null) {
+    //    Thread.sleep(3000)
+    //    println("So... what is you're prediction?")
+    //}
+
+}
+
+fun rollDice (){
+    val numbers = arrayOf(1, 2, 3, 4, 5, 6)
+    val randomNumber = numbers.random()
+    println(randomNumber)
+    if (randomNumber < 4) {
+        println("low")
+    } else {
+        println("low")
+    }
 }
 
 fun gameOver(){
